ef3420bce141f48f5c95d314677e64b0159131dd
CraftNoteBook <mcbijiben@foxmail.com>
2016-10-22 03:12:19 +0800

    Update HangingBreakByEntityEvent.java

--- BukkitApi/org/bukkit/event/hanging/HangingBreakByEntityEvent.java
+++ BukkitApi/org/bukkit/event/hanging/HangingBreakByEntityEvent.java
@@ -4,7 +4,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Hanging;
 
 /**
- * 当一个悬挂实体被一个实体移除时调用本事件
+ * 当一个物品展示框被一个实体破坏时调用本事件
  */
 public class HangingBreakByEntityEvent extends HangingBreakEvent {
     private final Entity remover;
@@ -15,11 +15,11 @@ public class HangingBreakByEntityEvent extends HangingBreakEvent {
     }
 
     /**
-     * 获取移除这个悬挂实体的实体.
+     * 获取破坏这个物品展示框的实体.
      * <p>
      * 原文：Gets the entity that removed the hanging entity
      *
-     * @return 移除这个悬挂实体的实体
+     * @return 破坏这个物品展示框的实体
      */
     public Entity getRemover() {
         return remover;
